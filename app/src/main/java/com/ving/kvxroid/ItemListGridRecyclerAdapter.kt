2b37cf4f2092f634cf512474db45db38226b3c9f
ChuyenDo <hanhchuyen6791@yahoo.com>
2019-10-04 21:50:59 +0700

    add detail activity

--- app/src/main/java/com/ving/kvxroid/ItemListGridRecyclerAdapter.kt
+++ app/src/main/java/com/ving/kvxroid/ItemListGridRecyclerAdapter.kt
@@ -1,27 +1,47 @@
 package com.ving.kvxroid
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
 
 
-class ItemListGridRecyclerAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ItemListGridRecyclerAdapter: RecyclerView.Adapter<ItemListGridRecyclerAdapter.ItemViewHolder>() {
 //    private var listOfMovies = listOf<MovieModel>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return ItemListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_grid_movie, parent, false))
+    var onItemClick: (() -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_grid_movie, parent, false))
     }
+
+
 
 //    override fun getItemCount(): Int = listOfMovies.size
     override fun getItemCount(): Int = 5
 
 
-    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
-        val viewHolder = viewHolder as ItemListViewHolder
+    override fun onBindViewHolder(viewHolder: ItemViewHolder, position: Int) {
+//        val viewHolder = viewHolder as ItemListViewHolder
 //        movieViewHolder.bindView(listOfMovies[position])
-        viewHolder.bindView()
+//        viewHolder.bindView()
     }
 
     fun setList() {
         notifyDataSetChanged()
+    }
+
+    inner class ItemViewHolder(
+        override val containerView: View
+    ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+
+        init {
+            containerView.setOnSafeClickListener {
+                onItemClick?.invoke()
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/ving/kvxroid/MainActivity.kt
+++ app/src/main/java/com/ving/kvxroid/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,9 +22,19 @@ class MainActivity : AppCompatActivity() {
 //        //This will for default android divider
 //        recyclerViewMovies.addItemDecoration(GridItemDecoration(10, 2))
 
-        val itemListAdapter = ItemListGridRecyclerAdapter()
+        val itemListAdapter = ItemListGridRecyclerAdapter().apply {
+            onItemClick = ::handleItemClick
+        }
         listRecyclerView.adapter = itemListAdapter
+
 //        movieListAdapter.setMovieList(generateDummyData())
         itemListAdapter.setList()
     }
+
+    private fun handleItemClick() {
+        println("Hello")
+        val intent = Intent(this, ItemDetailActivity::class.java)
+        startActivity(intent)
+    }
+
 }
