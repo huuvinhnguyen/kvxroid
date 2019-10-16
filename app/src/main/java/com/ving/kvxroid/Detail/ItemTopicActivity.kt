470a9807f9dd59f54ad7864f7e745c4378c6cf66
Bin Nguyen <nguyen.huu.vinh@moneyforward.vn>
2019-10-16 16:21:08 +0700

    add toast alert

--- app/src/main/java/com/ving/kvxroid/Detail/ItemTopicActivity.kt
+++ app/src/main/java/com/ving/kvxroid/Detail/ItemTopicActivity.kt
@@ -2,13 +2,25 @@ package com.ving.kvxroid.Detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import com.ving.kvxroid.R
+import com.ving.kvxroid.setOnSafeClickListener
+import kotlinx.android.synthetic.main.activity_item_topic.*
 
 class ItemTopicActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_topic)
+        btnConnect.setOnSafeClickListener {
+            val text = "Connect fails!"
+            val duration = Toast.LENGTH_SHORT
 
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.setGravity(Gravity.CENTER_VERTICAL or Gravity.LEFT, 0, 0)
+
+            toast.show()
+        }
     }
 }
