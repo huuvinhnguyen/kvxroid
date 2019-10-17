d5ced7ff559f95f6ca01e3e99a99f7bb1546e5d0
ChuyenDo <hanhchuyen6791@yahoo.com>
2019-10-18 00:06:11 +0700

    add selection feature

--- app/src/main/java/com/ving/kvxroid/Detail/ItemTopicActivity.kt
+++ app/src/main/java/com/ving/kvxroid/Detail/ItemTopicActivity.kt
@@ -1,10 +1,12 @@
 package com.ving.kvxroid.Detail
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
 import com.ving.kvxroid.R
+import com.ving.kvxroid.Selection.SelectionActivity
 import com.ving.kvxroid.setOnSafeClickListener
 import kotlinx.android.synthetic.main.activity_item_topic.*
 
@@ -21,6 +23,11 @@ class ItemTopicActivity : AppCompatActivity() {
             toast.setGravity(Gravity.CENTER_VERTICAL or Gravity.LEFT, 0, 0)
 
             toast.show()
+        }
+
+        btnSelect.setOnSafeClickListener {
+            val intent = Intent(this, SelectionActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/ving/kvxroid/Selection/SelectionServerViewModel.kt
@@ -0,0 +1,6 @@
+package com.ving.kvxroid.Selection
+
+import com.ving.kvxroid.AnyObject
+import com.ving.kvxroid.extensions.empty
+
+data class SelectionServerViewModel(val title: String = String.empty()) : AnyObject
