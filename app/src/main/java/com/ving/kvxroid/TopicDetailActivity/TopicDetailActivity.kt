461064bc904f6d32ed68a40921c06f714d55f826
Bin Nguyen <nguyen.huu.vinh@moneyforward.vn>
2019-10-22 16:00:32 +0700

    add topic detail 2 viewholders type

--- app/src/main/java/com/ving/kvxroid/TopicDetailActivity/TopicDetailActivity.kt
+++ app/src/main/java/com/ving/kvxroid/TopicDetailActivity/TopicDetailActivity.kt
@@ -2,12 +2,33 @@ package com.ving.kvxroid.TopicDetailActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ving.kvxroid.R
+import kotlinx.android.synthetic.main.activity_topic_detail.*
+
 
 class TopicDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic_detail)
+        initView()
+    }
+
+    private fun initView() {
+
+//        //This will for default android divider
+//        recyclerViewMovies.addItemDecoration(GridItemDecoration(10, 2))
+        recyclerView.layoutManager =  LinearLayoutManager(this@TopicDetailActivity)
+
+
+        val itemListAdapter = TopicDetailAdapter(ArrayList()).apply {
+
+        }
+
+        recyclerView.adapter = itemListAdapter
+
+//        movieListAdapter.setMovieList(generateDummyData())
+        itemListAdapter.setItems()
     }
 }
--- app/src/main/java/com/ving/kvxroid/TopicDetailActivity/TopicDetailAdapter.kt
+++ app/src/main/java/com/ving/kvxroid/TopicDetailActivity/TopicDetailAdapter.kt
@@ -1,22 +1,17 @@
 package com.ving.kvxroid.TopicDetailActivity
 
-import com.ving.kvxroid.Detail.ItemDetailBaseViewHolder
-import com.ving.kvxroid.Detail.ItemDetailHeaderViewModel
-import com.ving.kvxroid.Detail.ItemDetailPlusViewModel
-import com.ving.kvxroid.Detail.ItemDetailSwitchViewModel
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ving.kvxroid.R
 import com.ving.kvxroid.setOnSafeClickListener
-import kotlinx.android.extensions.LayoutContainer
 import com.ving.kvxroid.AnyObject
-import kotlinx.android.synthetic.main.activity_item_detail_header.view.textView
-import kotlinx.android.synthetic.main.item_detail_plus_view_holder.view.*
+import kotlinx.android.synthetic.main.topic_detail_server_view_holder.view.*
+import kotlinx.android.synthetic.main.topic_detail_server_view_holder.view.textView
+import kotlinx.android.synthetic.main.topic_detail_view_holder.view.*
 
-class ItemDetailRecyclerAdapter(private val items: ArrayList<AnyObject>): RecyclerView.Adapter<ItemDetailBaseViewHolder<*>>() {
+class TopicDetailAdapter(private val items: ArrayList<AnyObject>): RecyclerView.Adapter<TopicDetailBaseViewHolder<*>>() {
 
     companion object {
         private const val TYPE_TOPIC = 0
@@ -28,22 +23,21 @@ class ItemDetailRecyclerAdapter(private val items: ArrayList<AnyObject>): Recycl
     private var data: List<Any> = emptyList()
 
 
-    //    private var listOfMovies = listOf<MovieModel>()
     var onItemClick: (() -> Unit)? = null
     var onItemPlusClick: ((String) -> Unit)? = null
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemDetailBaseViewHolder<*> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopicDetailBaseViewHolder<*> {
         return when (viewType) {
             TYPE_TOPIC -> {
                 val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_detail_header_view_holder, parent, false)
-                return HeaderViewHolder(view)
+                    .inflate(R.layout.topic_detail_view_holder, parent, false)
+                return TopicDetailViewHolder(view)
             }
             TYPE_SERVER -> {
                 val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_detail_switch_view_holder, parent, false)
-                return SwitchViewHolder(view)
+                    .inflate(R.layout.topic_detail_server_view_holder, parent, false)
+                return TopicDetailServerViewHolder(view)
             }
             else -> throw IllegalArgumentException("Invalid view type")
 
@@ -52,12 +46,12 @@ class ItemDetailRecyclerAdapter(private val items: ArrayList<AnyObject>): Recycl
 
     override fun getItemCount(): Int = items.size
 
-    override fun onBindViewHolder(viewHolder: ItemDetailBaseViewHolder<*>, position: Int) {
+    override fun onBindViewHolder(viewHolder: TopicDetailBaseViewHolder<*>, position: Int) {
 
         val element = items[position]
         when (viewHolder) {
-            is HeaderViewHolder -> viewHolder.bind(element as ItemDetailHeaderViewModel)
-            is SwitchViewHolder -> viewHolder.bind(element as ItemDetailSwitchViewModel)
+            is TopicDetailViewHolder -> viewHolder.bind(element as TopicDetailViewModel)
+            is TopicDetailServerViewHolder -> viewHolder.bind(element as TopicDetailServerViewModel)
             else -> throw IllegalArgumentException()
         }
     }
@@ -65,35 +59,22 @@ class ItemDetailRecyclerAdapter(private val items: ArrayList<AnyObject>): Recycl
     override fun getItemViewType(position: Int): Int {
         val comparable = items[position]
         return when (comparable) {
-            is ItemDetailHeaderViewModel -> TYPE_TOPIC
-            is ItemDetailSwitchViewModel -> TYPE_SERVER
+            is TopicDetailViewModel -> TYPE_TOPIC
+            is TopicDetailServerViewModel -> TYPE_SERVER
             else -> throw IllegalArgumentException("Invalid type of data " + position)
         }
     }
 
     fun setItems() {
-//        items.add(ItemDetailHeaderViewModel("Header abc"))
-//        items.add(ItemDetailSwitchViewModel("switch 1"))
-//        items.add(ItemDetailSwitchViewModel("switch 2"))
-//        items.add(ItemDetailPlusViewModel())
+        items.add(TopicDetailViewModel("Topic Detail"))
+        items.add(TopicDetailServerViewModel("Server 1 1"))
+
 
         notifyDataSetChanged()
     }
 
-    inner class ItemViewHolder(
-        override val containerView: View
-    ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-
-
-        init {
-            containerView.setOnSafeClickListener {
-                onItemClick?.invoke()
-            }
-        }
-
-    }
-
-    inner class HeaderViewHolder(itemView: View) : ItemDetailBaseViewHolder<ItemDetailHeaderViewModel>(itemView) {
+    inner class TopicDetailViewHolder(itemView: View) :
+        TopicDetailBaseViewHolder<TopicDetailViewModel>(itemView) {
 
         init {
             itemView.setOnSafeClickListener {
@@ -101,30 +82,27 @@ class ItemDetailRecyclerAdapter(private val items: ArrayList<AnyObject>): Recycl
             }
         }
 
-        override fun bind(viewModel: ItemDetailHeaderViewModel) {
-            itemView.textView.text = viewModel.title
+        override fun bind(item: TopicDetailViewModel) {
+//            itemView.textView = item.name
+            itemView.textView.text = item.name
         }
+
     }
 
-    inner class SwitchViewHolder(itemView: View) : ItemDetailBaseViewHolder<ItemDetailSwitchViewModel>(itemView) {
-
-        override fun bind(viewModel: ItemDetailSwitchViewModel) {
-            itemView.textView.text = viewModel.name
-        }
-    }
-
-    inner class PlusViewHolder(
-        override val containerView: View
-
-    ) : ItemDetailBaseViewHolder<ItemDetailPlusViewModel>(containerView), LayoutContainer {
+    inner class TopicDetailServerViewHolder(itemView: View) :
+        TopicDetailBaseViewHolder<TopicDetailServerViewModel>(itemView) {
 
         init {
-            containerView.imageButton.setOnSafeClickListener {
-                onItemPlusClick?.invoke("Click Plus")
+            itemView.setOnSafeClickListener {
+                onItemClick?.invoke()
             }
         }
 
-        override fun bind(viewModel: ItemDetailPlusViewModel) {}
+        override fun bind(item: TopicDetailServerViewModel) {
+            itemView.textView.text = item.name
+
+        }
     }
+
 
 }
